4d6ebc20d43b8096b71460e2499781de628c5264
Pranav Chitiveli <39918567+PVCPipe@users.noreply.github.com>
2022-02-12 19:21:06 -0500

    first code run by not me wooooooooo!

--- src/main/java/frc/robot/commands/IndexCommand.java
+++ src/main/java/frc/robot/commands/IndexCommand.java
@@ -25,7 +25,7 @@ public class IndexCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    index.set(-0.1);
+    index.set(0.1);
   }
   // Called once the command ends or is interrupted.
   @Override
--- src/main/java/frc/robot/commands/IntakeCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommand.java
@@ -25,7 +25,7 @@ public class IntakeCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    intake.set(-0.1);
+    intake.set(0.1);
   }
 
   // Called once the command ends or is interrupted.
